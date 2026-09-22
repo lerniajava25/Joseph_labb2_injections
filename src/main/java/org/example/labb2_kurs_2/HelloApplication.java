511e8a1717cf9e74255db76765a71f229c4f904c
tavla75 <joseph.lindetun@gmail.com>
2026-09-22 15:46:18 +0200

    first commit

---
+++ src/main/java/org/example/labb2_kurs_2/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.labb2_kurs_2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
